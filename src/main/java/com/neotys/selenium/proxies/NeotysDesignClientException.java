d0ab29ca0a7c19f471aa553168dcbb994f7161c0
Sébastien Richert <sebastien.richert@neotys.com>
2020-07-24 22:26:19 -0400

    Add missing exception classes.

---
+++ src/main/java/com/neotys/selenium/proxies/NeotysDesignClientException.java
@@ -0,0 +1,10 @@
+package com.neotys.selenium.proxies;
+
+/**
+ * Created by anouvel on 08/01/2018.
+ */
+public class NeotysDesignClientException extends RuntimeException {
+	public NeotysDesignClientException(final Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/main/java/com/neotys/selenium/proxies/NeotysWrappingException.java
@@ -0,0 +1,10 @@
+package com.neotys.selenium.proxies;
+
+/**
+ * Created by anouvel on 08/01/2018.
+ */
+public class NeotysWrappingException extends RuntimeException {
+	public NeotysWrappingException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+}
